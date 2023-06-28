9b800079a55ebf7c180f07cbe409d23f586d2510
Neil R <136622827+neilroyNM@users.noreply.github.com>
2023-06-28 18:08:30 +0000

    Update 2 files

--- triangle.ts
+++ triangle.ts
@@ -1,1 +1,19 @@
-export class Triangle {}
+export class Triangle {
+    private sideA: Number;
+    private sideB: Number;
+    private sideC: Number;
+    
+    public constructor(sideA: Number, sideB: Number, sideC: Number) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+    
+    isEquilateral(isEquilateral: any): boolean {
+        if(this.sideA == this.sideB && this.sideA == this.sideC
+        && this.sideB == this.sideC){
+            return true;
+        }
+        return false;
+    }
+}
